6d52d4a32aa9bb282ac27890cb8c8c46bfad3648
Eddie Antonio Santos <easantos@ualberta.ca>
2020-09-04 13:04:13 -0600

    Remove sudo

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 # use with running services
 debug-frontend:
-	sudo docker-compose exec frontend bash
+	docker-compose exec frontend bash
 
 # use with running services
 debug-backend:
-	sudo docker-compose exec backend bash
+	docker-compose exec backend bash
 
 decompress-toy-corpus:
 	./volume-utils.py decompress
--- volume-utils.py
+++ volume-utils.py
@@ -6,7 +6,7 @@ import json
 import sys
 
 
-inspect_volume_args = ["sudo", "docker", "volume", "inspect", "korp-docker_corpora"]
+inspect_volume_args = ["docker", "volume", "inspect", "korp-docker_corpora"]
 print(" ".join(inspect_volume_args))
 p = subprocess.run(inspect_volume_args, check=True, encoding='utf-8', stdout=PIPE)
 docker_output = p.stdout
@@ -17,10 +17,10 @@ mount_path = a[0]['Mountpoint']
 
 
 if sys.argv[1] == 'decompress':
-    decompress_args = ["sudo", "tar", "xf", "Dickens-1.0.tar.xz", "-C", mount_path, '--strip', '1']
+    decompress_args = ["tar", "xf", "Dickens-1.0.tar.xz", "-C", mount_path, '--strip', '1']
     print(" ".join(decompress_args))
     p = subprocess.run(decompress_args, check=True, encoding='utf-8')
 elif sys.argv[1] == 'clear':
-    clear_args = ["sudo", "rm", "-rf", str(Path(mount_path) / "*")]
+    clear_args = ["rm", "-rf", str(Path(mount_path) / "*")]
     print(" ".join(clear_args))
     p = subprocess.run(clear_args, check=True, encoding='utf-8')
